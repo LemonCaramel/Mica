5c289c9e99eea3123eb55fd4ae4fb73cfdbe9667
LemonCaramel <admin@caramel.moe>
2024-08-13 19:45:27 +0900

    Support Minecraft 1.20.5+

--- neoforge/src/main/java/moe/caramel/mica/neoforge/MicaNeoForge.java
+++ neoforge/src/main/java/moe/caramel/mica/neoforge/MicaNeoForge.java
@@ -1,22 +1,31 @@
 package moe.caramel.mica.neoforge;
 
 import moe.caramel.mica.Mica;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import org.jetbrains.annotations.NotNull;
 
 @Mod(Mica.MOD_ID)
 public final class MicaNeoForge {
 
     public MicaNeoForge() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-    }
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> new IConfigScreenFactory() {
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> {
-            return new ConfigScreenHandler.ConfigScreenFactory((client, screen) -> Mica.configScreen(screen));
+            // ~ 1.20.6
+            @NotNull
+            public Screen createScreen(final @NotNull Minecraft minecraft, final @NotNull Screen screen) {
+                return Mica.configScreen(screen);
+            }
+
+            // 1.21 ~
+            @Override
+            public @NotNull Screen createScreen(final @NotNull ModContainer container, final @NotNull Screen screen) {
+                return Mica.configScreen(screen);
+            }
         });
     }
 }
